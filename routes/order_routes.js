da479920e580feaed1b4fcfdba4bc45731848b42
clara <10088152+claraj@users.noreply.github.com>
2022-01-26 14:33:15 -0600

    add delete orders link

--- routes/order_routes.js
+++ routes/order_routes.js
@@ -1,5 +1,6 @@
 let createError = require('http-errors');
 let express = require('express');
+const { render } = require('../app');
 let sqlite3 = require('sqlite3').verbose()
 let router = express.Router();
 
@@ -37,7 +38,7 @@ router.get('/order-list', function(req, res, next) {
 
 /* GET order edit page. Find order to edit in the database, 
 create a page with a form already populated with that order's information */
-router.get('/edit/:id', function(req, res, next){
+router.get('/edit/:id', function(req, res, next) {
   let orderId = req.params.id
   db.get("SELECT * FROM orders WHERE id = ?", orderId, function(err, order) {  // get first matching row - selecting by primary key so expect 1 or none.
     if (!order) {
@@ -49,12 +50,13 @@ router.get('/edit/:id', function(req, res, next){
 
 
 /* POST edit an order */
-router.post('/edit-order', function(req, res, next){
+router.post('/edit-order', function(req, res, next) {
 
   console.log(req.body)  // this is the form data,
   //note the names from the form elements are used to label the data, and the hidden element is included too 
   
-  db.run('UPDATE orders SET name = ?, product = ?, quantity = ? WHERE id = ?', [req.body.name, req.body.product, req.body.quantity, req.body.id], function(err) {
+  db.run('UPDATE orders SET name = ?, product = ?, quantity = ? WHERE id = ?', 
+  [req.body.name, req.body.product, req.body.quantity, req.body.id], function(err) {
     // Redirect to the order list page 
     return res.redirect('/order-list')
     // TODO as with all the other database routes, validation, and error handling
@@ -62,5 +64,15 @@ router.post('/edit-order', function(req, res, next){
 
 })
 
+router.get('/clear-orders', function(req, res, next) {
+  return res.render('delete_orders')
+})
+
+
+router.post('/clear-orders', function(req, res, next) {
+  db.run('DELETE FROM orders', function(err, order) {
+    return res.redirect('/')
+  })
+})
 
 module.exports = router;
